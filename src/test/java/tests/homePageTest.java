9e3a25237cde11d069f606b55dff2d777de05854
asya <asyacool922@gmail.com>
2024-04-25 12:19:44 +0300

    TC_14_01 updated

--- src/test/java/tests/homePageTest.java
+++ src/test/java/tests/homePageTest.java
@@ -14,6 +14,13 @@ public class homePageTest {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @Test
+    public void testHomePage() throws Exception {
+        Driver.getDriver().get(ConfigReader.getProperty("pick"));
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Pick"));
+
+    }
+
     @FindBy(id = "headlessui-menu-button-1")
     WebElement grocery;
 }
