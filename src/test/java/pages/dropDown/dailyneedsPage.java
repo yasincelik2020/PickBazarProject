32bf864118d84f2550ea84744445358250c49f21
rasit <sivaslirasit@gmail.com>
2024-04-25 15:40:32 +0200

    TC_14_01 ubdate

--- src/test/java/pages/dropDown/dailyneedsPage.java
+++ src/test/java/pages/dropDown/dailyneedsPage.java
@@ -3,14 +3,34 @@ package pages.dropDown;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import pages.homePage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class dailyneedsPage {
-    public dailyneedsPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public dailyneedsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    //"You deserve to eat fresh" yazisi görüntülenir.
+    @FindBy(xpath = "//h1[.=\"You Deserve To Eat Fresh\"]")
+    public WebElement dailyNeedsYouDeserve;
 
     //"We source the best healthy foods for you." locater
     @FindBy(xpath = "//p[.=\"We source the best healthy foods for you.\"]")
-    public WebElement dailyNeedsText;
+    public WebElement dailyNeedsWeSourse;
+
+    //"Search" secenegi olan frame görüntülenir.
+    @FindBy(id = "search")
+    public WebElement dailyNeedsWeSearchFrame;
+
+    public void dailyNeedsPage() {
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+        homePage homePage = new homePage();
+        homePage.homePageDropDownMenu.click();
+        homePage.dailyNeedsDropDownMenu.click();
+    }
+
+    //Aranan ürün Daily Needs ile ilgili ise ve sitede varsa gelmelidir
+
 }
--- src/test/java/pages/homePage.java
+++ src/test/java/pages/homePage.java
@@ -5,11 +5,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-
-
-
-
-
 public class homePage {
 
     public homePage() {
@@ -23,6 +18,7 @@ public class homePage {
 
     @FindBy(xpath = "//img[@alt='Pickbazar']")
     public WebElement PickBazarButon ;
+
 
 
 }
