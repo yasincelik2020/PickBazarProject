ccd6f563ddca4a0d2ff9a0911ae7c1b28b66a72f
beyza <csrbyz@gmail.com>
2024-04-29 10:01:51 +0200

    grocery page uptade

--- src/test/java/pages/dropDown/groceryPage.java
+++ src/test/java/pages/dropDown/groceryPage.java
@@ -1,5 +1,27 @@
 package pages.dropDown;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pages.homePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class groceryPage {
+    public groceryPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    public void groceryPageGit() {// page methodu
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+        homePage homePage = new homePage();
+        homePage.homePageDropDownMenu.click();
+        homePage.groceryDropDownMenu.click();
+    }
+    @FindBy(xpath = "//button[contains(text(),'Search')]")
+    public WebElement groceryWeSearchButton;
+
+    @FindBy(xpath = "//span[.=\"Clementines\"]")
+    public WebElement clementinesUrunu;
+
 
 }
