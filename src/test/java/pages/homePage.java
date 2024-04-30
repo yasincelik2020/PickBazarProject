bd56373ea18128b6e823ae6605964e51dda28489
ebru <ebrualzey3@gmail.com>
2024-04-30 09:12:40 +0200

    TC_02_01 Update

--- src/test/java/pages/homePage.java
+++ src/test/java/pages/homePage.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.ConfigReader;
+import org.testng.Assert;
+import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -67,7 +71,8 @@ public class homePage {
 
 
 
-
+    @FindBy(xpath = "(//span[@class='whitespace-nowrap'])[1]")
+    public WebElement groceryDropDownMenuHomePage;
 
     @FindBy(xpath = "//a[.='Shops']")
     public WebElement shopsButtonHomePage;
@@ -104,98 +109,60 @@ public class homePage {
     @FindBy(xpath = "//li[@class='rounded-md py-1']")
     public List<WebElement> groceryAltMenuUrunleriHomePage;
 
+    @FindBy(xpath = "//h1[@class='text-2xl font-bold tracking-tight text-heading lg:text-4xl xl:text-5xl']")
+    public WebElement groceriesDeliveredIn90MinuteText;
 
+    @FindBy(xpath = "//h3[.='All Shops']")
+    public WebElement allShopsShopsPage;
 
+    public void pickBazarButonClickMethod() throws InterruptedException, IOException {
 
+        pickBazarButonHomePage.click();
+        Assert.assertTrue(groceriesDeliveredIn90MinuteText.isDisplayed());
+        ReusableMethods.takeScreenshot("Groceries Delivered in 90 Minute ");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @FindBy(xpath = "//article[1]//header[1]//button[1]//span[1]")
-    public WebElement applesAddButton;
-    //article[20]//header[1]//button[1]//span[1]
-    @FindBy(xpath = "//article[20]//header[1]//button[1]//span[1]")
-    public WebElement spinachAddButton;
-
-    @FindBy(xpath = "//span[starts-with(@class,'flex ltr')]")
-    public WebElement itemsNumber;
-    @FindBy(xpath = "(//span[starts-with(@class,'text-sm ')])[1]")
-    public WebElement applesPriceValue;
-
-    @FindBy(xpath = "(//span[starts-with(@class,'text-sm ')])[2]")
-    public WebElement spinachPriceValue;
-
-    @FindBy(xpath = "//div[@class='min-h-screen']//button[2]//*[name()='svg']")
-    public WebElement applesAddButtonPlusSign;
-
-    @FindBy(xpath = "(//button[starts-with(@class,'cursor-pointer p-2 ')])[1]")
-    public WebElement applesAddButtonMinusSign;
-    @FindBy(xpath = "//span[@class='flex ltr:ml-2 rtl:mr-2']")
-    public WebElement chartButtonAtrightItemCount;
-
-    @FindBy(xpath = "//span[@class='bg-light rounded w-full py-2 px-2 text-accent mt-3']")
-    public WebElement chartButtonAtrightPrice;
-
-    @FindBy(xpath = "//span[starts-with(@class,'flex h-full flex-1 ')]")
-    public WebElement checkoutButton;
-    public void chartAddFunctions(){
-        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
     }
 
-    public double valueEvuloation (WebElement apples, WebElement spinach){
-        double total = Double.parseDouble(apples.getText().substring(1))+Double.parseDouble(spinach.getText().substring(1));
-        return total;
-    }
+    @FindBy(xpath = "//h1[.='FAQ']")
+    public WebElement faqTextFAQPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/test/java/tests/homePageTest.java
+++ src/test/java/tests/homePageTest.java
@@ -15,6 +15,7 @@ import org.testng.Assert;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 import pages.homePage;
+import pages.loginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.JavaScriptExecutorUtils;
@@ -25,7 +26,8 @@ import java.io.IOException;
 import static utilities.Driver.driver;
 
 public class homePageTest {
-    @Description("Kayitsiz kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir ; \"PickBazar\" butonu görüntülenmeli    ")
+   @Severity(SeverityLevel.NORMAL)
+   @Description("Kayitsiz kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir ; \"PickBazar\" butonu görüntülenmeli    ")
     @Test (groups ={ "Smoke","No role"})
     public void TC_01_01() {
 
@@ -53,7 +55,7 @@ public class homePageTest {
         Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
 
         homePage homePage = new homePage();
-        Assert.assertTrue(homePage.homePageDropDownMenu.isDisplayed());
+        Assert.assertTrue(homePage.groceryDropDownMenuHomePage.isDisplayed());
 
         Driver.closeDriver();
 
@@ -142,7 +144,7 @@ public class homePageTest {
     @Severity(SeverityLevel.NORMAL)
     @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Express Delivery\" frame ' i  görüntülenmeli")
     @Test(groups = {"Smoke", "No role"})
-    public void TC_01_08() {
+    public void TC_01_08() throws IOException {
 
         Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
 
@@ -151,30 +153,17 @@ public class homePageTest {
         homePage homePage = new homePage();
         Assert.assertTrue(homePage.expressDeliveryFrameHomePage.isDisplayed());
 
-        Driver.closeDriver();
-
-    }
-
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Kayitsiz kullanici olarak anasayfada ekran asagi kaydirildiginda ekranda \"Express Delivery\" frame ' inde  \"Save Now\" buttonu görüntülenmeli")
-    @Test(groups = {"Smoke", "No role"})
-    public void TC_01_09() {
-
-        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
-
-        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
-
-        homePage homePage = new homePage();
-        Assert.assertTrue(homePage.expressDeliveryFrameHomePage.isDisplayed());
+        ReusableMethods.takeScreenshot("Express Delivery Frame");
 
         Driver.closeDriver();
 
     }
+
 
     @Severity(SeverityLevel.NORMAL)
     @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Cash On Delivery\"  frame ' i  görüntülenmeli")
     @Test(groups = {"Smoke", "No role"})
-    public void TC_01_10() {
+    public void TC_01_10() throws IOException {
 
         Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
 
@@ -183,30 +172,17 @@ public class homePageTest {
         homePage homePage = new homePage();
         Assert.assertTrue(homePage.cashOnDeliveryFrameHomePage.isDisplayed());
 
-        Driver.closeDriver();
-
-    }
-
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Cash On Delivery\"  frame ' inde  \"Save Now\" buttonu görüntülenmeli")
-    @Test(groups = {"Smoke", "No role"})
-    public void TC_01_11() {
-
-        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
-
-        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
-
-        homePage homePage = new homePage();
-        Assert.assertTrue(homePage.expressDeliveryFrameHomePage.isDisplayed());
+        ReusableMethods.takeScreenshot("Cash On Delivery Frame");
 
         Driver.closeDriver();
 
     }
+
 
     @Severity(SeverityLevel.NORMAL)
     @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Gift Voucher\" frame ' i  görüntülenmeli")
     @Test(groups = {"Smoke", "No role"})
-    public void TC_01_12() {
+    public void TC_01_12() throws IOException {
 
         Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
 
@@ -215,31 +191,18 @@ public class homePageTest {
         homePage homePage = new homePage();
         Assert.assertTrue(homePage.giftVoucherFrameHomePage.isDisplayed());
 
-        Driver.closeDriver();
-
-    }
-
-    @Severity(SeverityLevel.NORMAL)
-    @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Gift Voucher\" frame ' inde  \"Shop Cuopons\" buttonu görüntülenmeli")
-    @Test(groups = {"Smoke", "No role"})
-    public void TC_01_13() {
-
-        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
-
-        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
-
-        homePage homePage = new homePage();
-        Assert.assertTrue(homePage.expressDeliveryFrameHomePage.isDisplayed());
+        ReusableMethods.takeScreenshot("Gift Voucher Frame");
 
         Driver.closeDriver();
 
     }
+
 
     @Severity(SeverityLevel.NORMAL)
     @Description("Kayitsiz kullanici olarak ana sayfada scroll down yapildiginda;\n" +
             "Ekranin Sag tarafinda \"Grocery\" ürünleri gelmeli")
     @Test(groups = {"Smoke", "No role"})
-    public void TC_01_14() {
+    public void TC_01_14() throws IOException {
 
         Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
 
@@ -249,10 +212,14 @@ public class homePageTest {
         actions.sendKeys(Keys.PAGE_DOWN).perform(); // Bir sayfa aşağı kaydır
 
         homePage homePage = new homePage();
+        int k=0;
 
         for (WebElement listElement : homePage.groceryUrunleriHomePage) {
+            System.out.println(homePage.groceryUrunleriHomePage.get(k).getText());
            Assert.assertTrue(listElement.isDisplayed());
+           k++;
         }
+        ReusableMethods.takeScreenshot("Grocery ana sayfa urunleri");
 
         Driver.closeDriver();
 
@@ -288,12 +255,13 @@ public class homePageTest {
     @Severity(SeverityLevel.NORMAL)
     @Description(" Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir ; \"PickBazar\" butonu görüntülenmeli       ")
     @Test(groups = {"Smoke", "Customer"})
-    public void TC_01_16() {
+    public void TC_01_16() throws InterruptedException, IOException {
 
-        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+        ReusableMethods.takeScreenshot("Login");
 
         Assert.assertEquals(Driver.getDriver().getCurrentUrl(),ConfigReader.getProperty("pickUrl"));
-
 
         homePage homePage=new homePage();
         Assert.assertTrue( homePage.pickBazarButonHomePage.isDisplayed());
@@ -301,6 +269,327 @@ public class homePageTest {
         Driver.closeDriver();
 
     }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("  Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir ; " +
+            "Sol üstte dropdown menü otomatik olarak \"Grocery\" secili sekilde görüntülenmeli      ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_17() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.groceryDropDownMenuHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("  Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir:\"Shops\"  butonu görüntülenmeli  ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_18() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.shopsButtonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description(" Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir: \"Offers\" butonu görüntülenmeli   ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_19() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.offersButtonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description(" Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir: \"FAQ\"  butonu görüntülenmeli    ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_20() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.faqButtonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("  Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir: \"Become a Seller\" butonu görüntülenmeli    ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_21() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.becomeASellerButtonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description(" Kayitli kullanici olarak ana sayfanin elementlerinin dogru sekilde geldigi görüntülenmelidir: \"Join\" butonu görüntülenmeli    ")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_22() throws InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.joinButtonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitli kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Express Delivery\" frame ' i  görüntülenmeli")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_23() throws IOException, InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.expressDeliveryFrameHomePage.isDisplayed());
+
+        ReusableMethods.takeScreenshot("Express Delivery Frame");
+
+        Driver.closeDriver();
+
+    }
+
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitli kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Cash On Delivery\"  frame ' i  görüntülenmeli")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_25() throws IOException, InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.cashOnDeliveryFrameHomePage.isDisplayed());
+
+        ReusableMethods.takeScreenshot("Cash On Delivery Frame");
+
+        Driver.closeDriver();
+
+    }
+
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitsiz kullanici olarak ana sayfada ekran asagi kaydirildiginda ekranda \"Gift Voucher\" frame ' i  görüntülenmeli")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_27() throws IOException, InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        Assert.assertTrue(homePage.giftVoucherFrameHomePage.isDisplayed());
+
+        ReusableMethods.takeScreenshot("Gift Voucher Frame");
+
+        Driver.closeDriver();
+
+    }
+
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitli kullanici olarak ana sayfada scroll down yapildiginda;\n" +
+            "Ekranin Sag tarafinda \"Grocery\" ürünleri gelmeli")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_29() throws IOException, InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform(); // Bir sayfa aşağı kaydır
+
+        homePage homePage = new homePage();
+        int k=0;
+
+        for (WebElement listElement : homePage.groceryUrunleriHomePage) {
+            System.out.println(homePage.groceryUrunleriHomePage.get(k).getText());
+            Assert.assertTrue(listElement.isDisplayed());
+            k++;
+        }
+        ReusableMethods.takeScreenshot("Grocery ana sayfa urunleri");
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitli kullanici olarak ana sayfada scroll down yapildiginda;\n" +
+            "Ekranin Sol tarafinda \"Grocery\"nin alt menüleri gelmeli")
+    @Test(groups = {"Smoke", "Customer"})
+    public void TC_01_30() throws IOException, InterruptedException {
+
+        loginPage loginPage=new loginPage();
+        loginPage.loginMethod();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        homePage homePage = new homePage();
+        int i=0;
+
+        for (WebElement listElement : homePage.groceryAltMenuUrunleriHomePage) {
+            System.out.println(homePage.groceryAltMenuUrunleriHomePage.get(i).getText());
+            Assert.assertTrue(listElement.isDisplayed());
+            i++;
+        }
+        ReusableMethods.takeScreenshot("Grocery alt menu" );
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Istenilen sayfada \"PickBazar\" butonu olmali")
+    @Test
+    public void US_02_PreCondition() {
+
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage=new homePage();
+        Assert.assertTrue( homePage.pickBazarButonHomePage.isDisplayed());
+
+        Driver.closeDriver();
+
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitsiz kullanici olarak ana sayfada  \"Pick Bazar\" butonunun calistigi görülmeli")
+    @Test(groups = {"Regression", "No role"},dependsOnMethods = "US_02_PreCondition")
+    public void TC_02_01() throws IOException, InterruptedException {
+
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+
+       JavaScriptExecutorUtils.scrollDownByJS(Driver.getDriver());
+
+        homePage homePage = new homePage();
+        homePage.pickBazarButonClickMethod();
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitsiz kullanici olarak ,\"PickBazar\" butonunun oldugu Shops sayfasinda  \"PickBazar\" butonu ana sayfa ekranina dönmemizi saglamali.")
+    @Test(groups = {"Regression", "No role"})
+    public void TC_02_02() throws IOException, InterruptedException {
+
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        homePage.shopsButtonHomePage.click();
+
+        Assert.assertTrue(homePage.allShopsShopsPage.isDisplayed());
+
+        ReusableMethods.takeScreenshot("All Shops");
+
+        homePage.pickBazarButonClickMethod();
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitsiz kullanici olarak ,\"PickBazar\" butonunun oldugu Offers sayfasinda  \"PickBazar\" butonu ana sayfa ekranina dönmemizi saglamali.")
+    @Test(groups = {"Regression", "No role"})
+    public void TC_02_03() throws IOException, InterruptedException {
+
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        homePage.offersButtonHomePage.click();
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("offers"));
+
+        homePage.pickBazarButonClickMethod();
+
+        Driver.closeDriver();
+
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Kayitsiz kullanici olarak ,\"PickBazar\" butonunun oldugu FAQ sayfasinda  \"PickBazar\" butonu ana sayfa ekranina dönmemizi saglamali.")
+    @Test(groups = {"Regression", "No role"})
+    public void TC_02_04() throws IOException, InterruptedException {
+
+        Driver.getDriver().get(ConfigReader.getProperty("pickUrl"));
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("pickUrl"));
+
+        homePage homePage = new homePage();
+        homePage.faqButtonHomePage.click();
+
+        Assert.assertTrue(homePage.faqTextFAQPage.isDisplayed());
+
+        ReusableMethods.takeScreenshot("FAQ");
+
+        homePage.pickBazarButonClickMethod();
+
+        Driver.closeDriver();
+
+    }
+
+
+
+
+
 
     @Severity(SeverityLevel.NORMAL)
     @Description("Kullanici anasayfada Shelf dropdown menudeki seceneklerden birini sectiginde ürünün image i görüntülenmeli ")
